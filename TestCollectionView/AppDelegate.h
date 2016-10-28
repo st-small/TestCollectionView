0fa3a698326af0679737b3cde90da62ad6f87d70
Stanly Shiyanovskiy <st-small@rambler.ru>
2016-10-28 11:23:05 +0300

    class prefix SiS

---
+++ TestCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestCollectionView
+//
+//  Created by Stanly Shiyanovskiy on 28.10.16.
+//  Copyright © 2016 Stanly Shiyanovskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestCollectionView
+//
+//  Created by Stanly Shiyanovskiy on 28.10.16.
+//  Copyright © 2016 Stanly Shiyanovskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
